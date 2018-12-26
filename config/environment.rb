34f7229c324233839bc084dd56e2c120e23d8a76
Princeton Evans <princetonevans@yahoo.com>
2018-12-26 16:11:29 -0600

    changed env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,24 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'Princeton Evans'}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address:              'smtp.gmail.com',
+port:                 587,
+domain:               'Princetonevans.com',
+user_name:            'princetonevans5@gmail.com',
+password:             'beasting5',
+authentication:       'plain',
+enable_starttls_auto: true  }
